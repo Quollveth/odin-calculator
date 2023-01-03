978f2fc1b76d61b3a9898134df9914f93220d0e0
Quollveth <klauss.marrer@gmail.com>
2023-01-03 16:43:13 -0300

    almost working

--- script.js
+++ script.js
@@ -4,33 +4,77 @@ let operationDisplay = document.querySelector('.operation');
 let resultDisplay = document.querySelector('.result');
 
 //variables
-let currentNumber = ''; //number currently on display, string that gets parsed later
+let currentNumber = ''; //number currently on display
 let isDecimal = false; //does the current number have a decimal place?
+let value1 = null; //numbers to be evaluated
+let value2 = null;
+let operator = '';
+let operatorSet = false; //has the operator been set?
+let result = null;
+let operationDone = false; //is this a typed operation done or a repeat
+
+//MATH!
+function evaluate(value1,value2,operator){
+    switch(operator){
+        case '+':
+            return value1+value2;
+        case '-':
+            return value1-value2;
+        case '*':
+            return value1*value2;
+        case '/':
+            return value1/value2;
+    }
+}
 
 // handle the different buttons
 function handleNumbers(id){
-    if(isDecimal == false){
-        currentNumber += id!='decimal'?id:currentNumber==''?'0.':'.'; 
-        //append the current number to the end of currentNumber to build the number as buttons are pressed, if decimal is pressed add a decimal point, if the decimal is the first one add a leading zero
-        if(id == 'decimal'){
-            isDecimal = true; //so you can only add one decimal point
+    if(currentNumber.length <= 10){ //character limit
+        if(operationDone){ //if number is pressed after operation resets the calculator state
+            clearDisplay();
         }
-    } else if(id != 'decimal'){
-        currentNumber += id;
+        if(isDecimal == false){
+            currentNumber += id!='decimal'?id:currentNumber==''?'0.':'.'; 
+            //append the current number to the end of currentNumber to build the number as buttons are pressed, if decimal is pressed add a decimal point, if the decimal is the first one add a leading zero
+            if(id == 'decimal'){
+                isDecimal = true; //so you can only add one decimal point
+            }
+        } else if(id != 'decimal'){
+            currentNumber += id;
+        }
+        resultDisplay.value = currentNumber;
+        if(operatorSet){
+            value2 = parseFloat(currentNumber);
+        } else{
+            value1 = parseFloat(currentNumber);
+        }
     }
-    resultDisplay.value = currentNumber;
 }
 
 function handleOperator(id){
-
+    if(operationDone){
+        operationDone = false;
+    }
+    operatorSet = true;
+    isDecimal = false;
+    operator = id;
+    operationDisplay.value = `${value1} ${operator}`
+    resultDisplay.value = '';
+    currentNumber = '';
 }
 
 function clearDisplay(){
     //reset everything
-    operationDisplay.value = '';
     resultDisplay.value = '';
+    operationDisplay.value = '';
     currentNumber = '';
     isDecimal = false;
+    value1 = null;
+    value2 = null;
+    operator = '';
+    operatorSet = false;
+    result = null;
+    operationDone = false;
 }
 
 function backspace(){
@@ -38,11 +82,22 @@ function backspace(){
         isDecimal = false //if the decimal point is removed allow it to be placed again
     }
     currentNumber = currentNumber.slice(0,-1); //remove last number
+    //update display and values
     resultDisplay.value = currentNumber;
+    if(operatorSet){
+        value2 = parseFloat(currentNumber);
+    } else{
+        value1 = parseFloat(currentNumber);
+    }
 }
 
 function equal(){
-
+    result = evaluate(value1,value2,operator);
+    result = removeRightZeroes(result.toFixed(10));
+    operationDisplay.value = `${value1} ${operator} ${value2}`;
+    resultDisplay.value = result;
+    operationDone = true;
+    value1 = result;
 }
 
 //event handler on all buttons
@@ -65,6 +120,15 @@ function handleButtons(id){
                 handleNumbers(id);
         }
     }
+}
+
+function removeRightZeroes(num){
+    return parseFloat(num.toString().replace(/\.?0+$/,''));
+    /*
+    converts number to a string
+    finds any zeroes at the right of the decimal place that go to the end of the string and removes them
+    parses the string back into a float and returns it
+    */
 }
 
 //initialize the code
